8907ad36eab753fa0ba184a3cd497a2d1a1b2c4d
eugenegolovanov <eugene.golovanov@hart.com>
2016-05-12 17:25:19 -0700

    old style constraints added

--- demoProject/ExtendedVC+HartSpinner.swift
+++ demoProject/ExtendedVC+HartSpinner.swift
@@ -34,11 +34,25 @@ extension ExtendedVC {
         spinner.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(spinner)
         
-        let horizontalConstraint = spinner.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor)
-        let verticalConstraint = spinner.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor)
-        let widthConstraint = spinner.widthAnchor.constraintEqualToAnchor(nil, constant: spinnerFrame.size.width)
-        let heightConstraint = spinner.heightAnchor.constraintEqualToAnchor(nil, constant: spinnerFrame.size.height)
-        NSLayoutConstraint.activateConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+        //New Style Constraints
+//        let horizontalConstraint = spinner.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor)
+//        let verticalConstraint = spinner.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor)
+//        let widthConstraint = spinner.widthAnchor.constraintEqualToAnchor(nil, constant: spinnerFrame.size.width)
+//        let heightConstraint = spinner.heightAnchor.constraintEqualToAnchor(nil, constant: spinnerFrame.size.height)
+//        NSLayoutConstraint.activateConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+        
+        //Old Style Constraints
+        spinner.translatesAutoresizingMaskIntoConstraints = false//swift 2.0
+        let spinnerWidthConstraint = NSLayoutConstraint(item: spinner, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: spinnerFrame.size.width)
+        spinner.addConstraint(spinnerWidthConstraint)
+        let spinnerHeightConstraint = NSLayoutConstraint(item: spinner, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: spinnerFrame.size.height)
+        spinner.addConstraint(spinnerHeightConstraint)
+        let spinnerHorizontalConstraint = NSLayoutConstraint(item: spinner, attribute: NSLayoutAttribute.CenterX , relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0)
+        self.view.addConstraint(spinnerHorizontalConstraint)
+        let spinnerVerticalConstraint = NSLayoutConstraint(item: spinner, attribute: NSLayoutAttribute.CenterY , relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterY, multiplier: 1, constant: 0)
+        self.view.addConstraint(spinnerVerticalConstraint)
+
+
         
 
     }
